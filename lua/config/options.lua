3eca727e1f359b64d5b04eecce93f08259115b1a
hrllk <roovcloudy@gmail.com>
2024-03-25 22:03:18 +0900

    modify dot file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,8 @@
 local opt = vim.opt
 
 opt.ttimeoutlen = 0
+
+opt.tabstop = 4 -- A TAB character looks like 4 spaces
+opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+opt.shiftwidth = 4 -- Number of spaces inserted when indenting
