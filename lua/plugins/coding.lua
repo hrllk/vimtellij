6848007792a5e9ff585c619b8fa68b321344d77a
hrllk <roovcloudy@gmail.com>
2024-04-07 01:42:02 +0900

    modify options of rename plugin, update readme

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,9 +1,22 @@
 return {
 
-  -- rename
+  -- inc-rename
+  -- for rename (refactor)
   {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
+    keys = {
+      {
+        "<leader>rn",
+        function()
+          return ":IncRename" .. vim.fn.expand("<cword>")
+        end,
+        desc = "Incremental rename",
+        mode = "n",
+        noremap = true,
+        expr = true,
+      },
+    },
     config = true,
   },
 
