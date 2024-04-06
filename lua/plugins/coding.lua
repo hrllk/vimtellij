d24509fe97008e70b3e6d49783a3b9de2e240145
hrllk <roovcloudy@gmail.com>
2024-04-06 22:58:32 +0900

    disable copilot

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -8,17 +8,17 @@ return {
   },
 
   -- copilot
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
 }
