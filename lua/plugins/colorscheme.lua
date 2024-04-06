524ba3479c8743740cf40f625730c8b5d70fe845
hrllk <roovcloudy@gmail.com>
2024-04-06 22:21:04 +0900

    modify theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,12 +6,29 @@ return {
   --   },
   -- },
   --
+  -- tokyo night
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
+
+  --
+  -- {
+  --   "sainnhe/sonokai",
+  --   proiroty = 1000,
+  --   config = function()
+  --     vim.g.sonokai_trasparent_background = "1"
+  --     vim.g.sonokai_enable_italic = "1"
+  --     vim.g.sonokai_style = "andromeda"
+  --     vim.cmd.colorscheme("sonokai")
+  --   end,
+  -- },
   -- {
   --   "craftzdog/solarized-osaka.nvim",
   --   lazy = true,
