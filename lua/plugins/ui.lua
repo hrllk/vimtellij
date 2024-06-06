f3179ec6d1d4d1bb5221b74dfae191dd3acea295
hrllk <roovcloudy@gmail.com>
2024-06-07 08:03:14 +0900

    add plugin DBUI

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -87,6 +87,10 @@ return {
     end,
   },
 
+  {
+    "nvim-lualine/lualine.nvim",
+    enabled = false,
+  },
 
 
   -- -------------------------------------------------
@@ -145,4 +149,31 @@ return {
 
 
 
+  -- -------------------------------------------------
+  -- DB TOOL
+  -- -------------------------------------------------
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+		keys = {
+			{
+
+				"<leader>d",
+				"<cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
+			},
+		},
+	},
 }
