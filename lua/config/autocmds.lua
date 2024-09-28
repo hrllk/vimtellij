d945ae585d11c54f5192112f774212042a9f602b
hrllk <roovcloudy@gmail.com>
2024-09-28 14:20:15 +0900

    update dotfiles

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,4 +31,4 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 -- visual color
 vim.api.nvim_command("hi Visual guifg=#000000 guibg=#33FF00 gui=none")
 -- jdtls 초기화
-vim.fn.system("rm -rf ~/.cache/nvim/jdtls/")
+-- vim.fn.system("rm -rf ~/.cache/nvim/jdtls/")
