88e080a04d86f586606ceeb4031f0468d02ad510
hrllk <roovcloudy@gmail.com>
2025-01-30 01:04:40 +0900

    update dotfile
    remove neovide configuration

--- init.lua
+++ init.lua
@@ -10,15 +10,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- For init.lua
-vim.g.neovide_transparency = 0.93
-vim.g.neovide_cursor_vfx_mode = 'railgun'
-
-vim.g.neovide_cursor_vfx_mode = ''
-vim.g.neovide_cursor_animation_length = 0.05
-vim.g.neovide_cursor_trail_length = 0.0
-vim.g.neovide_no_idle = true
-
-
--- vim.o.guifont = "Monaco:h11"
-vim.o.guifont = "D2Coding:h11"
