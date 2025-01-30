0214c9aaed0a955b00cd78c131b79a15982dbcf6
hrllk <roovcloudy@gmail.com>
2025-01-30 22:13:32 +0900

    update dotfile
    add plugins about comment

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -254,4 +254,31 @@ return {
       })
     end,
   },
+
+  -- -------------------------------------------------
+  -- comment
+  -- for commenting
+  -- -------------------------------------------------
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+  },
+
+  -- -------------------------------------------------
+  -- todo comment
+  -- for todo commenting as below
+  -- TODO :,
+  -- WARN :...
+  -- -------------------------------------------------
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
 }
