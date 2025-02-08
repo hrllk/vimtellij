1c867fed7c22020fc59b3dd924e36fb1f3193acc
hr.kim <hr.kim@okestro.com>
2025-02-08 13:47:50 +0900

    update ai config
    revive chatgpt plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,53 +3,113 @@ return {
   -- -------------------------------------------------
   -- chatgpt
   -- -------------------------------------------------
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup(
+
+
+        {
+          openai_params = {
+          -- NOTE: model can be a function returning the model name
+          -- this is useful if you want to change the model on the fly
+          -- using commands
+          -- Example:
+          -- model = function()
+          --     if some_condition() then
+          --         return "gpt-4-1106-preview"
+          --     else
+          --         return "gpt-3.5-turbo"
+          --     end
+          -- end,
+          model = "gpt-3.5-turbo",
+          -- model = "gpt-4-1106-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 1024,
+          temperature = 0.2,
+          top_p = 0.1,
+          n = 1,
+        },
+
+        popup_input = {
+          submit = "<C-s>",
+        }
+      }
+      )
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  --
+  --
+  --
+  --
+  -- -------------------------------------------------
+  -- deepseek 
+  -- -------------------------------------------------
   -- {
-  --   "jackMort/ChatGPT.nvim",
+  --   "yetone/avante.nvim",
   --   event = "VeryLazy",
-  --   config = function()
-  --     require("chatgpt").setup(
-  --
-  --
-  --       {
-  --         openai_params = {
-  --         -- NOTE: model can be a function returning the model name
-  --         -- this is useful if you want to change the model on the fly
-  --         -- using commands
-  --         -- Example:
-  --         -- model = function()
-  --         --     if some_condition() then
-  --         --         return "gpt-4-1106-preview"
-  --         --     else
-  --         --         return "gpt-3.5-turbo"
-  --         --     end
-  --         -- end,
-  --         model = "gpt-3.5-turbo",
-  --         -- model = "gpt-4-1106-preview",
-  --         frequency_penalty = 0,
-  --         presence_penalty = 0,
-  --         max_tokens = 1024,
-  --         temperature = 0.2,
-  --         top_p = 0.1,
-  --         n = 1,
-  --       },
-  --
-  --       popup_input = {
-  --         submit = "<C-s>",
-  --       }
-  --     }
-  --     )
-  --   end,
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     provider = "openai",
+  --     auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+  --     openai = {
+  --       endpoint = "https://api.deepseek.com/v1",
+  --       model = "deepseek-chat",
+  --       timeout = 30000, -- Timeout in milliseconds
+  --       temperature = 0,
+  --       max_tokens = 4096,
+  --       -- optional
+  --       api_key_name = "OPENAI_API_KEY",  -- default OPENAI_API_KEY if not set
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
   --     "nvim-lua/plenary.nvim",
-  --     "folke/trouble.nvim",
-  --     "nvim-telescope/telescope.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
   --   },
   -- },
-  --
-  --
-  --
-  --
+ 
   -- -------------------------------------------------
   -- deepseek 
   -- -------------------------------------------------
@@ -59,16 +119,14 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "openai",
-      auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-      openai = {
-        endpoint = "https://api.deepseek.com/v1",
-        model = "deepseek-chat",
-        timeout = 30000, -- Timeout in milliseconds
-        temperature = 0,
-        max_tokens = 4096,
-        -- optional
-        api_key_name = "OPENAI_API_KEY",  -- default OPENAI_API_KEY if not set
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-reasoner",
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
