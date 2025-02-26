8dd8fcacb8d16e3989ef6fdeead040ce8366a404
hr.kim <hr.kim@okestro.com>
2025-02-26 18:26:58 +0900

    deepseek model change

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -125,7 +125,7 @@ return {
           __inherited_from = "openai",
           api_key_name = "DEEPSEEK_API_KEY",
           endpoint = "https://api.deepseek.com",
-          model = "deepseek-reasoner",
+          model = "deepseek-coder",
         },
       },
     },
