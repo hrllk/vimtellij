12a1ebd5a8c7bbb8aea5cbbbe36b74a92aad5ef6
hr.kim <hr.kim@okestro.com>
2025-05-24 20:57:11 +0900

    update neo tree configuration

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -282,4 +282,63 @@ return {
       -- refer to the configuration section below
     }
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- -------------------------------------------------
+  -- neo tree
+  -- -------------------------------------------------
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    lazy = false,
+    -----Instead of using `config`, you can use `opts` instead, if you'd like:
+    -----@module "neo-tree"
+    -----@type neotree.Config
+    --opts = {},
+    config = function()
+
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, -- only works on Windows for hidden files/directories
+            -- hide_by_name = {
+            --   --"node_modules"
+            -- },
+            -- hide_by_pattern = { -- uses glob style patterns
+            --   --"*.meta",
+            --   --"*/src/*/tsconfig.json",
+            -- },
+            -- always_show = { -- remains visible even if other settings would normally hide it
+            --   --".gitignored",
+            -- },
+            -- always_show_by_pattern = { -- uses glob style patterns
+            --   --".env*",
+            -- },
+            -- never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            --   --".DS_Store",
+            --   --"thumbs.db"
+            -- },
+            -- never_show_by_pattern = { -- uses glob style patterns
+            --   --".null-ls_*",
+            -- },
+          },
+        },
+      })
+      vim.keymap.set("n", "<leader>e", "<Cmd>Neotree reveal<CR>")
+    end,
+  },
 }
