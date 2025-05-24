43ee8347b8bc164dda760dda7adc984848a8b09e
hr.kim <hr.kim@okestro.com>
2025-05-24 16:42:37 +0900

    disalbe dbtool

--- lua/plugins/dev-tools.lua
+++ lua/plugins/dev-tools.lua
@@ -3,30 +3,30 @@ return {
   -- -------------------------------------------------
   -- DB TOOL
   -- -------------------------------------------------
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-		init = function()
-			-- Your DBUI configuration
-			vim.g.db_ui_use_nerd_fonts = 1
-		end,
-		keys = {
-			{
-
-				";d",
-				"<cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
-			},
-		},
-	},
+	-- {
+	-- 	"kristijanhusak/vim-dadbod-ui",
+	-- 	dependencies = {
+	-- 		{ "tpope/vim-dadbod", lazy = true },
+	-- 		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+	-- 	},
+	-- 	cmd = {
+	-- 		"DBUI",
+	-- 		"DBUIToggle",
+	-- 		"DBUIAddConnection",
+	-- 		"DBUIFindBuffer",
+	-- 	},
+	-- 	init = function()
+	-- 		-- Your DBUI configuration
+	-- 		vim.g.db_ui_use_nerd_fonts = 1
+	-- 	end,
+	-- 	keys = {
+	-- 		{
+	--
+	-- 			";d",
+	-- 			"<cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
+	-- 		},
+	-- 	},
+	-- },
 
   -- -------------------------------------------------
 	-- LazyGit integration with Telescope
