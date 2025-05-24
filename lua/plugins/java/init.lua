7a25291ceced9e6611fa818d330ea057714c6959
hr.kim <hr.kim@okestro.com>
2025-05-24 20:57:40 +0900

    modify nvim java configuration

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -40,27 +40,52 @@
 --   }
 -- }
 --
+-- return {
+--   'nvim-java/nvim-java',
+--   config = false,
+--   dependencies = {
+--     {
+--       'neovim/nvim-lspconfig',
+--       opts = {
+--         inlay_hints = { enabled = false },
+--         servers = {
+--           jdtls = {
+--             -- Your custom jdtls settings goes here
+--           },
+--         },
+--         setup = {
+--           jdtls = function()
+--             require('java').setup({
+--               -- Your custom nvim-java configuration goes here
+--             })
+--           end,
+--         },
+--       },
+--     },
+--   },
+-- }
 return {
-  'nvim-java/nvim-java',
-  config = false,
-  dependencies = {
-    {
-      'neovim/nvim-lspconfig',
-      opts = {
-        inlay_hints = { enabled = false },
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
-          },
-        },
-        setup = {
-          jdtls = function()
-            require('java').setup({
-              -- Your custom nvim-java configuration goes here
-            })
-          end,
-        },
-      },
-    },
-  },
+  {
+    "nvim-java/nvim-java",
+  }
+  -- {
+  --   "nvim-java/nvim-java",
+  --   dependencies = {
+  --     "nvim-java/lua-async-await",
+  --     "nvim-java/nvim-java-core",
+  --     "nvim-java/nvim-java-test",
+  --     "nvim-java/nvim-java-refactor",
+  --     "nvim-java/nvim-java-dap",
+  --     "mfussenegger/nvim-dap",
+  --   }
+  -- },
+  -- {
+  --   "JavaHello/spring-boot.nvim",
+  --   ft = "java",
+  --   dependencies = {
+  --     "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
+  --     "ibhagwan/fzf-lua", -- optional
+  --   },
+  -- }
+
 }
