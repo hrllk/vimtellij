bc3dba9a4b952e7099131e003d60af76a05b49c6
hrllk <roovcloudy@gmail.com>
2025-05-28 01:12:05 +0900

    add snack configuration
    1. rename
    2. colorschemes
    3. git branch sitch

---
+++ lua/plugins/snack.lua
@@ -0,0 +1,22 @@
+
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+    },
+    keys = {
+        { "<leader>rn", function() require("snacks").rename.rename_file() end, desc = "Fast Rename Current File"},
+        { "<leader>th", function() require("snacks").picker.colorschemes({ layout = "ivy" }) end, desc = "Pick Color Schemes"},
+        { "<leader>gbr", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc = "Pick and Switch Git Branch"},
+    },
+
+  },
+  -- {
+  --   "folke/todo-comments.nvim",
+  --   event = { "BufReadPre", "BufNewFile"},
+  --   keys = {
+  --     { "<leader>pt", function() require("snacks").picker.colorschemes({ layout = "ivy" }) end, desc = "Pick Color Schemes"},
+  --   }
+  -- }
+}
