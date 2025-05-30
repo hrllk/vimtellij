5261e505d4671ab1c7fe6ea43b3c7f5903331654
hr.kim <hr.kim@okestro.com>
2025-05-30 18:02:53 +0900

    init nvim-java configuration

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,91 +1,45 @@
 return {
   "nvim-java/nvim-java",
+  config = false,
   dependencies = {
-    "nvim-java/lua-async-await",
-    "nvim-java/nvim-java-refactor",
-    "nvim-java/nvim-java-core",
-    "nvim-java/nvim-java-test",
-    "nvim-java/nvim-java-dap",
-    "MunifTanjim/nui.nvim",
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
     {
-      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
       opts = {
-        registries = {
-          "github:nvim-java/mason-registry",
-          "github:mason-org/mason-registry",
+        servers = {
+          -- Your JDTLS configuration goes here
+          jdtls = {
+            -- settings = {
+            --   java = {
+            --     configuration = {
+            --       runtimes = {
+            --         {
+            --           name = "JavaSE-23",
+            --           path = "/usr/local/sdkman/candidates/java/23-tem",
+            --         },
+            --       },
+            --     },
+            --   },
+            -- },
+          },
+        },
+        setup = {
+          jdtls = function()
+            -- Your nvim-java configuration goes here
+            require("java").setup({
+              -- root_markers = {
+              --   "settings.gradle",
+              --   "settings.gradle.kts",
+              --   "pom.xml",
+              --   "build.gradle",
+              --   "mvnw",
+              --   "gradlew",
+              --   "build.gradle",
+              --   "build.gradle.kts",
+              -- },
+            })
+          end,
         },
       },
     },
-    -- {
-    --   "williamboman/mason-lspconfig.nvim",
-    --   opts = {
-    --     handlers = {
-    --       ["jdtls"] = function()
-    --         require("java").setup()
-    --       end,
-    --     },
-    --   },
-    -- },
   },
-  opts = {},
 }
---
---
--- return {
---   "nvim-java/nvim-java",
---   dependencies = {
---     "neovim/nvim-lspconfig",
---   }
--- }
---
--- return {
---   'nvim-java/nvim-java',
---   config = false,
---   dependencies = {
---     {
---       'neovim/nvim-lspconfig',
---       opts = {
---         inlay_hints = { enabled = false },
---         servers = {
---           jdtls = {
---             -- Your custom jdtls settings goes here
---           },
---         },
---         setup = {
---           jdtls = function()
---             require('java').setup({
---               -- Your custom nvim-java configuration goes here
---             })
---           end,
---         },
---       },
---     },
---   },
--- }
--- return {
---   {
---     "nvim-java/nvim-java",
---   }
-  -- {
-  --   "nvim-java/nvim-java",
-  --   dependencies = {
-  --     "nvim-java/lua-async-await",
-  --     "nvim-java/nvim-java-core",
-  --     "nvim-java/nvim-java-test",
-  --     "nvim-java/nvim-java-refactor",
-  --     "nvim-java/nvim-java-dap",
-  --     "mfussenegger/nvim-dap",
-  --   }
-  -- },
-  -- {
-  --   "JavaHello/spring-boot.nvim",
-  --   ft = "java",
-  --   dependencies = {
-  --     "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
-  --     "ibhagwan/fzf-lua", -- optional
-  --   },
-  -- }
-
--- }
