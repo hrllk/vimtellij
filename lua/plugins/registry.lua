4164e7e25f8c678348d56ff4dddca7af47f189af
hrllk <heykia3@protonmail.com>
2025-06-10 13:35:05 +0900

    modify registry configuration
    default registry 사용해야 jdtls install 가능

--- lua/plugins/registry.lua
+++ lua/plugins/registry.lua
@@ -1,17 +1,28 @@
 return {
-    -- {
-    --     "williamboman/mason.nvim",
-    --     config = function()
-    --         -- setup mason with default properties
-    --         require("mason").setup()
-    --     end
-    -- },
-
-  'williamboman/mason.nvim',
-  opts = {
-    registries = {
-      'github:nvim-java/mason-registry',
-      'github:mason-org/mason-registry',
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            -- setup mason with default properties
+            require("mason").setup()
+        end
     },
-  },
+
+  -- 'williamboman/mason.nvim',
+  -- opts = {
+  --   registries = {
+  --     'github:nvim-java/mason-registry',
+  --     'github:mason-org/mason-registry',
+  --   },
+  -- },
+  --
+  -- 'williamboman/mason.nvim',
+  -- config = function()
+  --     require("mason").setup({
+  --       registries = {
+  --         'github:nvim-java/mason-registry',
+  --         'github:mason-org/mason-registry', -- lombok 설치위한 임시 필요
+  --       },
+  -- })
+  -- end
+
 }
