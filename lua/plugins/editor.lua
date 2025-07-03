a70339589b2979dbcb29a4c97ff52bee938e7eba
hrllk <heykia3@protonmail.com>
2025-07-03 10:08:44 +0900

    update eiditor configuration
    1. add tab ui plugin
    2. modify snack's plugins (picker hidden configuration, ...)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -20,7 +20,16 @@ return {
   --   },
   --   config = true,
   -- },
-
+  -- -------------------------------------------------
+  -- tab
+  -- -------------------------------------------------
+  {
+    'nanozuki/tabby.nvim',
+    ---@type TabbyConfig
+    opts = {
+      -- configs...
+    },
+  },
 
   -- init.lua:
   -- -------------------------------------------------
@@ -30,37 +39,18 @@ return {
     "folke/snacks.nvim",
     opts = {
       image = {}, -- required magick (install with homebrew)
-    },
-  },
-  -- -------------------------------------------------
-  -- explorer (tree)
-  -- -------------------------------------------------
-  {
-    "folke/snacks.nvim",
-    opts = {
       explorer = {},
+      picker = {
+      },
     },
     keys = {
       { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-    }
-  },
-  -- -------------------------------------------------
-  -- finder (telescope)
-  -- -------------------------------------------------
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {}
-    },
-    keys = {
-      -- vim.keymap.set('n', '<leader>ff', '<cmd>lua Snacks.dashboard.pick("files")<CR>', { desc = '[F]ind [F]iles' })
-      { "<leader>fb", function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
       { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-      { "<leader>ff", function() Snacks.picker.files() end,                                   desc = "Find Files" },
-      { "<leader>fg", function() Snacks.picker.grep() end,                                    desc = "Grep" },
-      -- { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-      { "<leader>fp", function() Snacks.picker.projects() end,                                desc = "Projects" },
-      { "<leader>fr", function() Snacks.picker.recent() end,                                  desc = "Recent" },
+      { "<leader>ff", function() Snacks.picker.files({ hidden = true }) end, desc = "Find Files" }, -- hidden option
+      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     }
   },
 
