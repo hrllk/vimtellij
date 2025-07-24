9772c5b1c8390cb69186652c0985f1ecafac145a
hrllk <heykia3@protonmail.com>
2025-07-24 19:02:24 +0900

    enable rename plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,23 +3,23 @@ return {
   -- inc-rename
   -- for rename (refactor)
   -- -------------------------------------------------
-  -- {
-  --   "smjonas/inc-rename.nvim",
-  --   cmd = "IncRename",
-  --   keys = {
-  --     {
-  --       "<leader>rn",
-  --       function()
-  --         return ":IncRename " .. vim.fn.expand("<cword>")
-  --       end,
-  --       desc = "Incremental rename",
-  --       mode = "n",
-  --       noremap = true,
-  --       expr = true,
-  --     },
-  --   },
-  --   config = true,
-  -- },
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    keys = {
+      {
+        "<leader>rn",
+        function()
+          return ":IncRename " .. vim.fn.expand("<cword>")
+        end,
+        desc = "Incremental rename",
+        mode = "n",
+        noremap = true,
+        expr = true,
+      },
+    },
+    config = true,
+  },
   -- -------------------------------------------------
   -- tab
   -- -------------------------------------------------
