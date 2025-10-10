105eaaee893e4060c4c9864bd7fd9edfb000fab7
hrllk <roovcloudy@gmail.com>
2025-10-10 22:01:53 +0900

    update lsp configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "jdtls" },        -- mason registry needs
+        ensure_installed = { "lua_ls", "ts_ls" },        -- mason registry needs
       })
     end
   },
--- lua/plugins/registry.lua
+++ lua/plugins/registry.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      -- setup mason with default properties
-      require("mason").setup()
-    end
-  },
-
-  -- 'williamboman/mason.nvim',
-  -- opts = {
-  --   registries = {
-  --     'github:nvim-java/mason-registry',
-  --     'github:mason-org/mason-registry',
-  --   },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   config = function()
+  --     -- setup mason with default properties
+  --     require("mason").setup()
+  --   end
   -- },
+
+  'williamboman/mason.nvim',
+  opts = {
+    registries = {
+      'github:nvim-java/mason-registry',
+      'github:mason-org/mason-registry',
+    },
+  },
   --
   -- 'williamboman/mason.nvim',
   -- config = function()
