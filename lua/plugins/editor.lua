ebd67a69f30501f78c307961a4cca3036b383e30
hrllk <heykia3@protonmail.com>
2025-11-04 14:14:03 +0900

    add a plugin for markdown render (pretty)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -134,6 +134,19 @@ return {
     end
   },
 
+  -- -------------------------------------------------
+  -- Markdown Render
+  -- -------------------------------------------------
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
 
   -- since `vim.tbl_deep_extend`, can only merge editortables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
