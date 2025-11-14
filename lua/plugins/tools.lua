3eb0dad39c238bc7198df34aa16b35bc17737b20
hrllk <heykia3@protonmail.com>
2025-11-14 16:42:18 +0900

    add new plugin for rest api

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -90,4 +90,25 @@ return {
       end,
     },
   },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function (_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "http")
+      end,
+    }
+  }
+  -- {
+  --   "rest-nvim/rest.nvim",
+  --   dependencies = {
+  --     "nvim-neorocks/rocks.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     opts = function(_, opts)
+  --       opts.ensure_installed = opts.ensure_installed or {}
+  --       table.insert(opts.ensure_installed, "http")
+  --     end,
+  --   },
+  -- }
 }
