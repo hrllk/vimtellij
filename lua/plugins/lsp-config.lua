9de14adc6f76181cb03f06339103a7aa4db3f7f1
hr_k <89384517+hrllk@users.noreply.github.com>
2026-02-26 19:15:55 +0900

    Updated lsp-config.lua to remove lines 53-59 related to mfussenegger/nvim-jdtls

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,51 @@
+return {
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls" },        -- mason registry needs
+      })
+    end
+  },
+
+  -- LspInfo, LspLog
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+
+      local lspconfig = require("lspconfig")
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- setup the lua language server
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      -- setup the typescript language server
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "<leader>O", ":lua vim.lsp.buf.code_action()<CR>")
+      vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+      vim.keymap.set("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+      -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+      vim.keymap.set("n", "gr", function()
+        Snacks.picker.lsp_references({
+          include_declaration = true,
+          include_current = true,
+          layout = { preset = "select" },
+        })
+      end, { desc = "LSP References (Snacks Ivy)" })
+      vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
+      vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>")
+
+    end
+  },
+
+}
