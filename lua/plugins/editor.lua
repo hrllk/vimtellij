43c5bb88e495e07702683933e1d3b5825e5916ea
hrllk <heykia3@protonmail.com>
2026-06-26 16:14:39 +0900

    feat: add fold preview and persist folds

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -491,6 +491,35 @@ return {
     config = function()
       require("supermaven-nvim").setup({})
     end,
-  }
+  },
 
+  -- -------------------------------------------------
+  -- fold preview / folding
+  -- -------------------------------------------------
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    event = "VeryLazy",
+    init = function()
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+    config = function()
+      require("ufo").setup()
+
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+
+      vim.keymap.set("n", "K", function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = "Peek fold / hover" })
+    end,
+  }
 }
