0eba2cbbb627526221a2b270a486b7d801689d0a
hrllk <heykia3@protonmail.com>
2026-07-26 16:05:05 +0900

    config: highlight window separator on terminal focus

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,6 +1,7 @@
 return {
   accent = "#2DD4BF",
   border = "#0F766E",
+  inactive = "#52525B",
   none = "NONE",
   terminal = "#000000",
   terminal_inline = "#0B1220",
--- lua/plugins/ui/nvim-window-hightlighter.lua
+++ lua/plugins/ui/nvim-window-hightlighter.lua
@@ -10,6 +10,42 @@ return {
     opts = {
       highlight = colors.accent,
     },
-    event = { "WinLeave" },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("colorful-winsep").setup(opts)
+
+      local focused = true
+      local group = vim.api.nvim_create_augroup("ColorfulWinSepFocus", { clear = true })
+
+      local function apply_highlight()
+        vim.api.nvim_set_hl(0, "ColorfulWinSep", {
+          fg = focused and colors.accent or colors.inactive,
+          bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg,
+        })
+      end
+
+      vim.api.nvim_create_autocmd("FocusLost", {
+        group = group,
+        callback = function()
+          focused = false
+          apply_highlight()
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FocusGained", {
+        group = group,
+        callback = function()
+          focused = true
+          apply_highlight()
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = group,
+        callback = apply_highlight,
+      })
+
+      apply_highlight()
+    end,
   },
 }
